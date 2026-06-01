bc78d008608d7607d344d114a02564150bd6d115
sgajbi <sandeepgajbi@yahoo.com>
2026-06-01 21:53:39 +0800

    Extract holdings response orchestration

--- src/services/query_service/app/services/position_service.py
+++ src/services/query_service/app/services/position_service.py
@@ -12,22 +12,10 @@ from ..dtos.position_dto import (
 from ..repositories.position_repository import PositionRepository
 from .portfolio_validation import ensure_portfolio_exists
 from .position_history_reads import position_history_response
-from .position_holdings import (
-    assign_position_weights,
-    holdings_data_quality_status,
-    holdings_response_as_of_date,
-    latest_holdings_evidence_timestamp,
-    merge_snapshot_and_history_position_rows,
-    portfolio_position_rows_data,
-    portfolio_positions_response_data,
-    position_held_since_requests,
-)
 from .position_holdings_reads import (
     effective_holdings_read_as_of_date,
-    fallback_holdings_valuation_map,
-    holdings_position_source_rows,
-    holdings_support_evidence,
 )
+from .position_holdings_response import portfolio_holdings_response
 
 logger = logging.getLogger(__name__)
 
@@ -83,59 +71,8 @@ class PositionService:
             include_projected=include_projected,
         )
 
-        snapshot_results, history_results = await holdings_position_source_rows(
+        return await portfolio_holdings_response(
             repository=self.repo,
             portfolio_id=portfolio_id,
             effective_as_of_date=effective_as_of_date,
-        )
-
-        db_results, history_supplements, snapshot_security_ids = (
-            merge_snapshot_and_history_position_rows(
-                snapshot_results=snapshot_results,
-                history_results=history_results,
-            )
-        )
-        fallback_valuation_map = await fallback_holdings_valuation_map(
-            repository=self.repo,
-            portfolio_id=portfolio_id,
-            effective_as_of_date=effective_as_of_date,
-            db_results=db_results,
-            history_supplements=history_supplements,
-            snapshot_security_ids=snapshot_security_ids,
-        )
-
-        positions = portfolio_position_rows_data(
-            db_results=db_results,
-            snapshot_security_ids=snapshot_security_ids,
-            fallback_valuation_map=fallback_valuation_map,
-        )
-        assign_position_weights(positions)
-
-        held_since_requests = position_held_since_requests(
-            db_results=db_results,
-            positions=positions,
-        )
-
-        response_as_of_date = holdings_response_as_of_date(
-            effective_as_of_date=effective_as_of_date,
-            positions=positions,
-        )
-        latest_market_price_dates = await holdings_support_evidence(
-            repository=self.repo,
-            portfolio_id=portfolio_id,
-            positions=positions,
-            held_since_requests=held_since_requests,
-            response_as_of_date=response_as_of_date,
-        )
-        return portfolio_positions_response_data(
-            portfolio_id=portfolio_id,
-            positions=positions,
-            response_as_of_date=response_as_of_date,
-            data_quality_status=holdings_data_quality_status(
-                positions=positions,
-                history_supplements=history_supplements,
-                response_as_of_date=response_as_of_date,
-                latest_market_price_dates=latest_market_price_dates,
-            ),
-            latest_evidence_timestamp=latest_holdings_evidence_timestamp(db_results),
         )
