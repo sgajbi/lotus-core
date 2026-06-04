391f78806d4acd3e5a7cc8c7acd24a916425a531
sgajbi <sandeepgajbi@yahoo.com>
2026-06-04 18:16:28 +0800

    Format shared config guard fix

--- src/libs/portfolio-common/portfolio_common/health_server.py
+++ src/libs/portfolio-common/portfolio_common/health_server.py
@@ -6,5 +6,6 @@ from portfolio_common import config
 
 HEALTH_PROBE_BIND_HOST_ENV = config.HEALTH_PROBE_BIND_HOST_ENV
 
+
 def health_probe_bind_host() -> str:
     return config.load_health_probe_bind_host()
