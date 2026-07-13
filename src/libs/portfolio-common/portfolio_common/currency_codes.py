c5a3c92c8a252dbd29f7ff50703a2a368462f447
sgajbi <sandeepgajbi@yahoo.com>
2026-07-14 06:48:48 +0800

    refactor(common): retire currency compatibility path

--- src/libs/portfolio-common/portfolio_common/currency_codes.py
+++
@@ -1,5 +0,0 @@
-"""Transitional imports for currency normalization during domain package migration."""
-
-from .domain.currency import normalize_currency_code, normalize_optional_currency_code
-
-__all__ = ["normalize_currency_code", "normalize_optional_currency_code"]
