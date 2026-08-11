e57921d4b4f43af2513bb8e7657126af5b13c6e1
sgajbi <sandeepgajbi@yahoo.com>
2026-08-11 10:27:38 +0800

    feat(corporate-actions): consume ordered manifest facts

--- src/services/portfolio_transaction_processing_service/app/delivery/kafka/__init__.py
+++ src/services/portfolio_transaction_processing_service/app/delivery/kafka/__init__.py
@@ -9,6 +9,7 @@ from .booked_transaction_replay_request_mapper import (
     map_booked_transaction_replay_request,
     parse_booked_transaction_replay_request,
 )
+from .corporate_action_manifest_consumer import CorporateActionManifestConsumer
 from .fixed_income_book_cost_authority_consumer import FixedIncomeBookCostAuthorityConsumer
 from .fixed_income_book_cost_correction_replay_consumer import (
     FixedIncomeBookCostCorrectionReplayConsumer,
@@ -21,6 +22,7 @@ __all__ = [
     "BookedTransactionReplayRequest",
     "BookedTransactionReplayRequestConsumer",
     "BookedTransactionReplayRequestPayloadError",
+    "CorporateActionManifestConsumer",
     "FixedIncomeBookCostAuthorityConsumer",
     "FixedIncomeBookCostCorrectionReplayConsumer",
     "FixedIncomeBookCostCorrectionReplaySourceMissing",
