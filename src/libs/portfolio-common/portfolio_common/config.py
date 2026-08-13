938e05cf3a0a8cab6bf8fe8ff8ef08e0ab83d84b
sgajbi <sandeepgajbi@yahoo.com>
2026-08-14 02:51:48 +0800

    refactor(config): remove unused Mongo credential defaults

--- src/libs/portfolio-common/portfolio_common/config.py
+++ src/libs/portfolio-common/portfolio_common/config.py
@@ -73,15 +73,6 @@ POSTGRES_DB = os.getenv("POSTGRES_DB", "portfolio_db")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
-MONGO_INITDB_ROOT_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME", "admin")
-MONGO_INITDB_ROOT_PASSWORD = os.getenv("MONGO_INITDB_ROOT_PASSWORD", "password")
-MONGO_HOST = os.getenv("MONGO_HOST", "mongodb")
-MONGO_PORT = os.getenv("MONGO_PORT", "2717")
-MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "portfolio_state")
-MONGO_URL = (
-    f"mongodb://{MONGO_INITDB_ROOT_USERNAME}:{MONGO_INITDB_ROOT_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}"
-)
-
 # Kafka Configurations
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS_HOST") or os.getenv(
     "KAFKA_BOOTSTRAP_SERVERS", "kafka:9093"
